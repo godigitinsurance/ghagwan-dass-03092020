8d086738cc04a9d73a8566ec6b23891b036233a2
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-02 17:20:04 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Bhagwan Dass");
+$("#policyNumber").text("DOI7262731");
+$("#startDate").text("03-Sep-2020");
+$("#endDate").text("02-Sep-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08AX0570");
+$("#make").text("HERO HONDA");
+$("#model").text("PASSION PLUS");
+$("#vehicalSubType").text("DRUM");
+$("#engineNo").text("06E09C45182");
+$("#chassis").text("06E08M42320");
